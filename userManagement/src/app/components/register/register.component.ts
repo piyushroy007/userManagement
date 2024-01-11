bc084cf8af42ddfb579a62be3d06533c356f9476
piyushroy007 <piyush9051roy@gmail.com>
2024-01-12 01:39:16 +0530

    material module added user model created registeration form creation

--- userManagement/src/app/components/register/register.component.ts
+++ userManagement/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  constructor(private builder: FormBuilder, private store: Store) {
 
+  }
+
+  registerform = this.builder.group({
+    username: this.builder.control('', Validators.compose([Validators.required, Validators.minLength(5)])),
+    password: this.builder.control('', Validators.required),
+    confirmpassword: this.builder.control('', Validators.required),
+    name: this.builder.control('', Validators.required),
+    email: this.builder.control('', Validators.compose([Validators.required, Validators.email])),
+    phone: this.builder.control('', Validators.required),
+    gender: this.builder.control('male')
+  })
 }
---
+++ userManagement/src/app/store/Model/user.model.ts
@@ -0,0 +1,10 @@
+export interface Users{
+    username:string,
+    password:string,
+    name:string,
+    email:string,
+    phone:string,
+    role:string,
+    gender:string,
+    status:boolean
+}
