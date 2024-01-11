3f80aae168b65f8019a626985fb9f2d361b7d3b5
piyushroy007 <piyush9051roy@gmail.com>
2024-01-12 01:17:04 +0530

    routes added and dummy data created

--- userManagement/src/app/app-routing.module.ts
+++ userManagement/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerlistingComponent } from './components/customerlisting/customerlisting.component';
+import { HomeComponent } from './components/home/home.component';
+import { AssociatelistingComponent } from './components/associatelisting/associatelisting.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'associate',component:AssociatelistingComponent},
+  {path:'customer',component:CustomerlistingComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'login',component:LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
